d89406ce75779cc3796806ca7fdbe99a04a56b9f
Jordan-182 <jordan.p62@outlook.fr>
2024-03-10 15:08:06 +0100

    scriptAnswer

---
+++ app.js
@@ -0,0 +1,107 @@
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionContainer = document.querySelector(".option-container");
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
+
+// push the questions into availableQuestions Array
+function setAvailableQuestions(){
+    const totalQuestion = quiz.length;
+    for(let i=0; i<totalQuestion; i++){
+        availableQuestions.push(quiz[i])
+    }
+}
+
+// set question number and question and options
+function getNewQuestion(){
+    // set question number
+    questionNumber.innerHTML = "Question " + (questionCounter + 1) + " sur " + quiz.length;
+    // set question text
+    // get random question
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+    // get the position of 'questionIndex' from the availableQuestion Array;
+    const index1 = availableQuestions.indexOf(questionIndex);
+    // remove the 'questionIndex' from the availableQuestion Array, so the question doesn't repeat
+    availableQuestions.splice(index1,1);
+    // set options
+    // get the lenght of options
+    const optionLen = currentQuestion.options.length;
+    // push options into availableOptions Array
+    for(let i=0; i<optionLen; i++){
+        availableOptions.push(i)
+    }
+
+    optionContainer.innerHTML = '';
+
+    let animationDelay = 0.15;
+    // create options in html
+    for(let i=0; i<optionLen; i++){
+        // random option
+        const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+        // get the position of 'optionIndex' from the availableOptions
+        const index2 = availableOptions.indexOf(optionIndex);
+        // remove the 'optionIndex' from the availableOptions, so the options doesn't repeat
+        availableOptions.splice(index2,1);
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[optionIndex];
+        option.id = optionIndex;
+        option.style.animationDelay = animationDelay + 's';
+        animationDelay = animationDelay + 0.2;
+        option.className = "option";
+        optionContainer.appendChild(option);
+        option.setAttribute("onclick","getResult(this)");
+    }
+    questionCounter++
+}
+
+// get the result of current attempt question
+function getResult(element){
+    const id = parseInt(element.id);
+    // get the answer by comparing the id of clicked option
+    if(id === currentQuestion.answer){
+        // set the green color to correct option
+        element.classList.add("correct");
+    }
+    else{
+        // set the red color to incorrect option
+        element.classList.add("wrong");
+
+        // if answer incorrect then show correct answer green
+        const optionLen = optionContainer.children.length;
+        for(let i=0; i<optionLen; i++){
+            if(parseInt(optionContainer.children[i].id) === currentQuestion.answer){
+                optionContainer.children[i].classList.add("correct");
+            }
+        }
+    }
+    unclickableOptions();
+}
+
+// make all the options unclickable once the user selected an option
+function unclickableOptions(){
+    const optionLen = optionContainer.children.length;
+    for(let i=0 ; i<optionLen ; i++){
+        optionContainer.children[i].classList.add("already-answered");
+    }
+}
+
+function next(){
+    if(questionCounter === quiz.length){
+        console.log("Quiz terminé!")
+    }
+    else{
+        getNewQuestion();
+    }
+}
+
+window.onload = function(){
+    // first set all questions in availableQuestions Array
+    setAvailableQuestions();
+    // then call getNewQuestion(); function
+    getNewQuestion();
+}
